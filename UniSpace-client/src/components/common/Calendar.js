5a96fdfec6f657122ed3e3fa2a0b3cae809f1092
jyj030000 <105355979+JYJ-0605@users.noreply.github.com>
2024-09-24 13:52:53 +0900

    Create Calendar.js

---
+++ UniSpace-client/src/components/common/Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Calendar = () => {
+  return (
+    <div className="calendar">
+      <h3>Calendar</h3>
+      <img src="calendar_image_url" alt="Calendar" />
+    </div>
+  );
+};
+
+export default Calendar;
