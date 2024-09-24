b15f6af37fbb497bd677e6e37869f01ed2042a50
jyj030000 <105355979+JYJ-0605@users.noreply.github.com>
2024-09-24 13:54:08 +0900

    Create Tasks.js

---
+++ UniSpace-client/src/components/common/Tasks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Tasks = () => {
+  return (
+    <div className="tasks">
+      <h3>Tasks</h3>
+      <ul>
+        <li>Complete Chapter 5 Reading</li>
+        <li>Submit Lab Report</li>
+        <li>Group Meeting at 4 PM</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Tasks;
