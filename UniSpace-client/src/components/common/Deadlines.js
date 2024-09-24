3fd738549d56f7c6350f394dea460963be4e1362
jyj030000 <105355979+JYJ-0605@users.noreply.github.com>
2024-09-24 13:53:18 +0900

    Create Deadlines.js

---
+++ UniSpace-client/src/components/common/Deadlines.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UpcomingDeadlines = () => {
+  return (
+    <div className="upcoming-deadlines">
+      <h3>Upcoming Deadlines</h3>
+      <ul>
+        <li>Assignment 1: Due on Oct 10, 2023</li>
+        <li>Project Proposal: Due on Oct 15, 2023</li>
+        <li>Mid-term Exam: Scheduled for Oct 20, 2023</li>
+      </ul>
+    </div>
+  );
+};
+
+export default UpcomingDeadlines;
