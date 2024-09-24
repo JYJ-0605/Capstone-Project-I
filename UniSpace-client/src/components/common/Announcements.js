7f02322db997870340bb5d1c4678b95407ca70a9
jyj030000 <105355979+JYJ-0605@users.noreply.github.com>
2024-09-24 13:51:43 +0900

    Update Announcements.js

--- UniSpace-client/src/components/common/Announcements.js
+++ UniSpace-client/src/components/common/Announcements.js
@@ -1,1 +1,16 @@
+import React from 'react';
 
+const Announcements = () => {
+  return (
+    <div className="announcements">
+      <h3>Announcements</h3>
+      <ul>
+        <li>New course materials available in the library.</li>
+        <li>Campus will be closed on Oct 25 for maintenance.</li>
+        <li>Join the webinar on 'Career Opportunities' on Oct 30.</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Announcements;
